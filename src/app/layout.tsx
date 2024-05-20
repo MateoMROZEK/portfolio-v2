617d0e6e845ba9b0bc6e947092e9e3350e297426
KilioZ <trix-folloow@hotmail.com>
2024-05-20 18:33:00 +0200

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,106 +1,165 @@
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-import './assets/css/globals.css'
-import './assets/libs/devicon/devicon.min.css'
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./assets/css/globals.css";
+import "./assets/libs/devicon/devicon.min.css";
 
-const montserrat_thin = Montserrat({ weight: ['100'], style: ['normal'], subsets: ['latin'] })
-const montserrat_regular = Montserrat({ weight: ['400'], style: ['normal'], subsets: ['latin'] })
-const montserrat_semiblack = Montserrat({ weight: ['700'], style: ['normal'], subsets: ['latin'] })
-const montserrat_black = Montserrat({ weight: ['900'], style: ['normal'], subsets: ['latin'] })
+import Image from "next/image";
+import Script from "next/script";
+
+const montserrat_thin = Montserrat({
+  weight: ["100"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+const montserrat_regular = Montserrat({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+const montserrat_semiblack = Montserrat({
+  weight: ["700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+const montserrat_black = Montserrat({
+  weight: ["900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr" className={montserrat_regular.className}>
       <body className="bg-mateo-gradient text-white">
-      <header className="skewY-0 z-[1000] relative">
-  <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-    <a href="/" className="-m-1.5 p-1.5">
-      <span className="sr-only">Mateo</span>
-      <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-    </a>
-    <div className="flex lg:hidden">
-      <button type="button" id="mobileMenuButton" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white">
-        <span className="sr-only">Open main menu</span>
-        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-      </button>
-    </div>
-    <div className="hidden lg:flex lg:gap-x-12">
-      <a href="/#a-propos" className="text-xl font-bold leading-6 text-white">A Propos</a>
-      <a href="/portfolio" className="text-xl font-bold leading-6 text-white">Portfolio</a>
-    </div>
-  </nav>
-  {/* Mobile menu, show/hide based on menu open state. */}
-  <div id="mobileMenu" className="hidden lg:hidden" role="dialog" aria-modal="true">
-    {/* Background backdrop, show/hide based on slide-over state. */}
-    <div className="fixed inset-0 z-10"></div>
-    <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 h-[100vh]">
-      <div className="flex items-center justify-between">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Mateo</span>
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt=""
-          />
-        </a>
-        <button type="button" id="closeMobileMenuButton" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-          <span className="sr-only">Close menu</span>
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
+        <header className="skewY-0 z-[1000] relative">
+          <nav
+            className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
-      <div className="mt-6 flow-root">
-        <div className="-my-6 divide-y divide-gray-500/10">
-          <div className="space-y-2 py-6">
-            <a
-              href="/#a-propos"
-              className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-            >
-              A Propos
+            <a href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Mateo</span>
+              <Image
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
             </a>
-            <a
-              href="/portfolio"
-              className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-            >
-              Portfolio
-            </a>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                id="mobileMenuButton"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+              <a
+                href="/#a-propos"
+                className="text-xl font-bold leading-6 text-white"
+              >
+                A Propos
+              </a>
+              <a
+                href="/portfolio"
+                className="text-xl font-bold leading-6 text-white"
+              >
+                Portfolio
+              </a>
+            </div>
+          </nav>
+          {/* Mobile menu, show/hide based on menu open state. */}
+          <div
+            id="mobileMenu"
+            className="hidden lg:hidden"
+            role="dialog"
+            aria-modal="true"
+          >
+            {/* Background backdrop, show/hide based on slide-over state. */}
+            <div className="fixed inset-0 z-10"></div>
+            <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 h-[100vh]">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Mateo</span>
+                  <Image
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt=""
+                  />
+                </a>
+                <button
+                  type="button"
+                  id="closeMobileMenuButton"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                >
+                  <span className="sr-only">Close menu</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    <a
+                      href="/#a-propos"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      A Propos
+                    </a>
+                    <a
+                      href="/portfolio"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Portfolio
+                    </a>
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Discord
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="py-6">
-            <a
-              href="#"
-              className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-            >
-              Discord
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-      {children}
-      
-      
-      <script src="/assets/js/main.js?v=2"></script>
+        </header>
+        {children}
+
+        <Script src="/assets/js/main.js?v=2"></Script>
       </body>
     </html>
-  )
+  );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,36 +1,110 @@
-import { Montserrat } from 'next/font/google';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
+import { Montserrat } from "next/font/google";
+import dynamic from "next/dynamic";
+import Link from "next/link";
 
-import { fetchData } from './../../../utils/api';
-import Image from 'next/image';
-import './../assets/css/globals.css';
-import './../assets/libs/devicon/devicon.min.css';
+import Image from "next/image";
 
-const montserrat_thin = Montserrat({ weight: ['100'], style: ['normal'], subsets: ['latin'] });
-const montserrat_regular = Montserrat({ weight: ['400'], style: ['normal'], subsets: ['latin'] });
-const montserrat_semiblack = Montserrat({ weight: ['700'], style: ['normal'], subsets: ['latin'] });
-const montserrat_black = Montserrat({ weight: ['900'], style: ['normal'], subsets: ['latin'] });
+import { fetchData } from "./../../../utils/api";
+import "./../assets/css/globals.css";
+import "./../assets/libs/devicon/devicon.min.css";
 
-import type { Metadata } from 'next'
+const montserrat_thin = Montserrat({
+  weight: ["100"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+const montserrat_regular = Montserrat({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+const montserrat_semiblack = Montserrat({
+  weight: ["700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+const montserrat_black = Montserrat({
+  weight: ["900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Mateo M. - Portfolio',
-    description: 'Découvrez mon portfolio, là où je montre tous les projets auxquels j\'ai participé. Développeur Web depuis 2013, j\'ai réalisé de nombreux projets, mais seuls les plus gros projets sont affichés.',
+  title: "Mateo M. - Portfolio",
+  description:
+    "Découvrez mon portfolio, là où je montre tous les projets auxquels j'ai participé. Développeur Web depuis 2013, j'ai réalisé de nombreux projets, mais seuls les plus gros projets sont affichés.",
+};
+
+export interface Root {
+  projects: Project[];
+  reviews: Review[];
+  staffs: Staff[];
+  clients: string[];
+  technologies: Technology[];
+}
+
+export interface Project {
+  name: string;
+  slug: string;
+  image: string;
+  released: boolean;
+  domain_url: string;
+  release_type: string;
+  release_date: string;
+  lite_description: string;
+  description: string;
+  categorie: string[];
+  changelog?: Changelog[];
+  technologie: string[];
+  columns: number;
+  type_project: string;
+  other_images?: string[];
+}
+
+export interface Changelog {
+  name: string;
+  date?: string;
+  list: List[];
+}
+
+export interface List {
+  text: string;
+}
+
+export interface Review {
+  name: string;
+  job: string;
+  content: string;
+  image: string;
+  note: string[];
+}
+
+export interface Staff {
+  name: string;
+  job: string;
+  icon: string;
+}
+
+export interface Technology {
+  name: string;
+  slug: string;
+  icon: string;
 }
 
 async function getData() {
-    const res = await fetchData('https://mateo.cybrico.de/api.json')
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-    return res
+  const res = await fetchData("https://mateo.cybrico.de/api.json");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  return res;
 }
 
 export default async function PortfolioPage() {
-    const data = await getData();
-    return (
+  const data = (await getData()) as Root;
+  return (
     <main className={montserrat_regular.className}>
-        <div className="flex flex-col gap-32 my-48 max-w-[1520px] px-10 mx-auto flex-1 w-full skewY-0">
+      <div className="flex flex-col gap-32 my-48 max-w-[1520px] px-10 mx-auto flex-1 w-full skewY-0">
         <div className="relative p-5">
           <span className={`${montserrat_thin.className} prenom`}>Mateo</span>
           <span className={`${montserrat_black.className} nom`}>MROZEK</span>
@@ -43,22 +117,54 @@ export default async function PortfolioPage() {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {data.projects.map((project, index) => (
-            <div key={index} className={`relative group overflow-hidden bg-[#7e2e2a] hover:bg-[#33201f] rounded-[40px] w-full flex items-center justify-center z-0 col-span-1 aspect-square cursor-pointer ${project.columns === 1 ? '' : 'lg:col-span-2 lg:aspect-auto'}`} tabIndex={0} style={{ transform: 'none' }}>
-                <canvas id={project.slug} className="absolute inset-0 z-[-1] w-full h-full opacity-0 group-hover:opacity-100 transition-all duration-300" width="1024" height="600"></canvas>
+              <div
+                key={index}
+                className={`relative group overflow-hidden bg-[#7e2e2a] hover:bg-[#33201f] rounded-[40px] w-full flex items-center justify-center z-0 col-span-1 aspect-square cursor-pointer ${
+                  project.columns === 1 ? "" : "lg:col-span-2 lg:aspect-auto"
+                }`}
+                tabIndex={0}
+                style={{ transform: "none" }}
+              >
+                <canvas
+                  id={project.slug}
+                  className="absolute inset-0 z-[-1] w-full h-full opacity-0 group-hover:opacity-100 transition-all duration-300"
+                  width="1024"
+                  height="600"
+                ></canvas>
                 <div className="absolute top-6 -right-full bg-white rounded-full p-2 group-hover:right-6 transition-all duration-300">
                   <Link key={index} href={`/project/${project.slug}`}>
-                    
-                      <img alt="arrow" fetchPriority="high" width="1000" height="1000" decoding="async" data-nimg="1" className="w-7 h-7" src="/assets/icons/arrow.svg" style={{ color: 'transparent' }} />
-                    
+                    <Image
+                      alt="arrow"
+                      fetchPriority="high"
+                      width="1000"
+                      height="1000"
+                      decoding="async"
+                      data-nimg="1"
+                      className="w-7 h-7"
+                      src="/assets/icons/arrow.svg"
+                      style={{ color: "transparent" }}
+                    />
                   </Link>
                 </div>
-                <img alt="logo" loading="lazy" width="1000" height="1000" decoding="async" data-nimg="1" className="h-28 w-auto max-w-[200px] object-contain" src={`${project.image}&v=1`} style={{ color: 'transparent' }} />
-                <div className="absolute bottom-5 text-white text-sm text-center uppercase">{project.name}</div>
-            </div>
+                <Image
+                  alt="logo"
+                  loading="lazy"
+                  width="1000"
+                  height="1000"
+                  decoding="async"
+                  data-nimg="1"
+                  className="h-28 w-auto max-w-[200px] object-contain"
+                  src={`${project.image}&v=1`}
+                  style={{ color: "transparent" }}
+                />
+                <div className="absolute bottom-5 text-white text-sm text-center uppercase">
+                  {project.name}
+                </div>
+              </div>
             ))}
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
